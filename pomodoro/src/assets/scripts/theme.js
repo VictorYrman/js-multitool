12ceb1bd09eff2ade85a14196760f0083c58554a
Victor Yrman <vitiayrashevich1256@gmail.com>
2025-05-13 10:16:11 +0300

    Change styles

--- pomodoro/src/assets/scripts/theme.js
+++ pomodoro/src/assets/scripts/theme.js
@@ -1,3 +1,20 @@
 export const changeTheme = (theme) => {
   document.body.dataset.theme = theme;
+  updateBrowserThemeColor(theme);
+};
+
+const updateBrowserThemeColor = (theme) => {
+  const metaThemeColor = document.querySelector("#theme-color");
+
+  switch (theme) {
+    case "pomodoro":
+      metaThemeColor.setAttribute("content", "#b94646");
+      break;
+    case "short":
+      metaThemeColor.setAttribute("content", "#3fa6a6");
+      break;
+    case "long":
+      metaThemeColor.setAttribute("content", "#386e94");
+      break;
+  }
 };
